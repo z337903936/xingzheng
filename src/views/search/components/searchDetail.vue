38eac944e49ca2526b52c7b197aff9af494960c1
Warren <493698299@qq.com>
2019-11-23 00:13:22 +0800

    修复现勘显示bug

--- src/views/search/components/searchDetail.vue
+++ src/views/search/components/searchDetail.vue
@@ -2,21 +2,21 @@
     <div>
         <el-row :gutter="20" class="mb10">
             <el-col :span="6">
-                勘查开始：{{ list.examBeginTime*1000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}') }}
+                勘查开始：{{ list.examBeginTime===''?(list.examBeginTime*1000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}')):'' }}
             </el-col>
             <el-col :span="6">
-                勘查结束：{{ list.examEndTime*1000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}') }}
+                勘查结束：{{list.examEndTime===''?(list.examEndTime*1000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}')):'' }}
             </el-col>
             <el-col :span="6">
-                案件开始：{{ list.caseBeginTime*1000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}') }}
+                案件开始：{{list.caseBeginTime===''?(list.caseBeginTime*1000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}')):''}}
             </el-col>
             <el-col :span="6">
-                案件结束：{{ list.caseEndTime*1000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}') }}
+                案件结束：{{ list.caseEndTime===''?(list.caseEndTime*1000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}')):'' }}
             </el-col>
         </el-row>
         <el-row :gutter="20" class="mb10">
             <el-col :span="6">
-                发案日期：{{ list.caseHappenTime*1000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}') }}
+                发案日期：{{  list.caseHappenTime===''?(list.caseHappenTime*1000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}')):''}}
             </el-col>
             <el-col :span="6">
                 发案区划：{{ list.caseHappenRegion }}
